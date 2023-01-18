8ee7d69f8356d166393c3c0d6c4c69bfafdacae1
samratalshahriar <samratalshahriar@gmail.com>
2023-01-19 00:44:46 +0600

    project setup

---
+++ android/app/src/main/kotlin/dev/groupy/alaapchari/alaapchari_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.groupy.alaapchari.alaapchari_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:alaapchari_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'screens/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Alaapchari',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Homepage(),
+    );
+  }
+}
